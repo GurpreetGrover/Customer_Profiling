70f1a1d669cc810459583dcfb23a15fd99724513
Gurpreet Singh Grover <gurpreet.grover01@live.com>
2024-08-22 20:10:39 +0530

    Added files

---
+++ transactions.sql
@@ -0,0 +1,58 @@
+create table transactions (
+	transaction_id INT PRIMARY KEY,
+	customer_id INT,
+	transaction_date DATE,
+	amount INT,
+	product VARCHAR(50),
+	cateogry VARCHAR(50)
+);
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (201, 111, '10/10/2023', 79, 'Pork - Tenderloin, Fresh', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (202, 131, '1/20/2024', 35, 'Bread - French Baquette', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (203, 137, '8/14/2023', 32, 'Cheese - Bocconcini', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (204, 104, '6/29/2024', 42, 'Beef - Top Sirloin', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (205, 117, '1/15/2024', 81, 'C - Plus, Orange', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (206, 109, '8/8/2024', 86, 'Ginger - Ground', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (207, 141, '2/9/2024', 47, 'Mushroom Morel Fresh', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (208, 105, '5/18/2024', 66, 'Soup - Knorr, Chicken Noodle', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (209, 113, '11/17/2023', 77, 'Bread Base - Gold Formel', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (210, 117, '3/20/2024', 16, 'Southern Comfort', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (211, 110, '8/2/2024', 94, 'Foil Wrap', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (212, 104, '1/16/2024', 31, 'Cheese - Sheep Milk', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (213, 131, '2/15/2024', 42, 'Island Oasis - Banana Daiquiri', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (214, 130, '10/20/2023', 68, 'Quail - Whole, Bone - In', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (215, 124, '8/9/2023', 24, 'Extract - Lemon', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (216, 109, '3/13/2024', 96, 'Bag Stand', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (217, 117, '10/1/2023', 60, 'Grapes - Black', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (218, 144, '11/14/2023', 76, 'Wine - White, Schroder And Schyl', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (219, 117, '12/15/2023', 47, 'Garbag Bags - Black', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (220, 116, '3/19/2024', 42, 'Yoplait Drink', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (221, 121, '7/16/2024', 93, 'Lumpfish Black', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (222, 117, '4/28/2024', 48, 'Crab - Claws, 26 - 30', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (223, 145, '8/6/2024', 10, 'Sausage - Andouille', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (224, 107, '1/30/2024', 37, 'Hersey Shakes', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (225, 112, '6/13/2024', 54, 'Coffee Cup 8oz 5338cd', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (226, 139, '5/10/2024', 81, 'Bread - Pita', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (227, 112, '1/8/2024', 52, 'Tea - Jasmin Green', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (228, 119, '11/1/2023', 97, 'True - Vue Containers', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (229, 147, '7/25/2023', 17, 'Lamb - Leg, Bone In', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (230, 146, '5/28/2024', 99, 'Table Cloth 53x53 White', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (231, 131, '7/23/2023', 91, 'Yogurt - Blueberry, 175 Gr', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (232, 128, '12/6/2023', 94, 'Lentils - Green, Dry', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (233, 147, '11/16/2023', 26, 'Beans - Fava Fresh', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (234, 140, '8/6/2024', 43, 'Beans - Kidney, Red Dry', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (235, 115, '7/7/2023', 71, 'Coffee - Decafenated', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (236, 125, '9/17/2023', 21, 'Dill Weed - Dry', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (237, 113, '9/16/2023', 21, 'Mushroom - White Button', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (238, 112, '8/22/2023', 13, 'Lidsoupcont Rp12dn', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (239, 127, '8/9/2023', 18, 'Lamb Rack - Ontario', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (240, 109, '5/26/2024', 42, 'Lamb - Leg, Boneless', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (241, 139, '6/18/2024', 25, 'Oven Mitts - 15 Inch', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (242, 105, '11/22/2023', 45, 'Food Colouring - Red', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (243, 101, '10/21/2023', 93, 'Lettuce - Red Leaf', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (244, 109, '7/28/2024', 46, 'Knife Plastic - White', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (245, 144, '10/5/2023', 56, 'Wine - Ruffino Chianti Classico', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (246, 101, '6/1/2024', 21, 'Ecolab - Medallion', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (247, 101, '12/15/2023', 91, 'Mace', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (248, 130, '3/20/2024', 74, 'Beef - Inside Round', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (249, 121, '6/8/2024', 64, 'Chocolate - Dark Callets', 'Grocery');
+insert into transactions (transaction_id, customer_id, transaction_date, amount, product, cateogry) values (250, 142, '4/25/2024', 47, 'Ecolab Crystal Fusion', 'Grocery');
